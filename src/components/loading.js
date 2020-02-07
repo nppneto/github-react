a94f7957abee93f31aa60bfa11857a98f5b32ab0
Nelson Neto <nelsonp2n@gmail.com>
2020-02-07 16:18:21 -0300

    Adicionando loading com gif

--- src/components/loading.js
+++ src/components/loading.js
@@ -3,7 +3,7 @@ import Spinner from '../assets/img/loading.gif';
 
 const Loading = () => (
     <div>
-        <img className="loading" src={Spinner} alt="" />
+        <img className="loading" src={ Spinner } alt="" />
     </div>
 );
 
